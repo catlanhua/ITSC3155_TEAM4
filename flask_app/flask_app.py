b1e652786b2fadde86be9070b5fca6414121e3d2
chua <chua@uncc.edu>
2021-11-08 11:53:57 -0500

    Added database functionality to store user and post information

--- flask_app/flask_app.py
+++ flask_app/flask_app.py
@@ -6,13 +6,18 @@ from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template
 from flask import request
 from flask import redirect, url_for
+from database import db
+from models import Post as Post
+from models import User as User
 
 app = Flask(__name__)     # create an app
-
-posts = {1: {'title': 'First post', 'text': 'This is my first post', 'date': '10-1-2020'},
-            2: {'title': 'Second post', 'text': 'This is my second post', 'date': '10-2-2020'},
-            3: {'title': 'Third post', 'text': 'This is my third post', 'date': '10-3-2020'},
-            }
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_qna_app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+#  Bind SQLAlchemy db object to this Flask app
+db.init_app(app)
+# Setup models
+with app.app_context():
+    db.create_all()   # run under the app context
 
 # @app.route is a decorator. It gives the function "index" special powers.
 # In this case it makes it so anyone going to "your-url/" makes this function
@@ -22,38 +27,40 @@ posts = {1: {'title': 'First post', 'text': 'This is my first post', 'date': '10
 @app.route('/')
 @app.route('/index')
 def index():
-   a_user = {'name': 'Ana Pathak', 'email': 'apathak@uncc.edu'}
-   return render_template('index.html', user=a_user)
+    a_user = db.session.query(User).filter_by(email='chua@uncc.edu').one()
+    return render_template('index.html', user=a_user)
 
 
 @app.route('/posts')
 def get_posts():
-   a_user = {'name': 'Ana Pathak', 'email': 'apathak@uncc.edu'}
-   return render_template('posts.html', posts=posts, user=a_user)
+    a_user = db.session.query(User).filter_by(email='chua@uncc.edu').one()
+    posts = db.session.query(Post).all()
+    return render_template('posts.html', posts=posts, user=a_user)
 
 
 @app.route('/posts/<post_id>')
 def get_post(post_id):
-   a_user = {'name': 'Ana Pathak', 'email': 'apathak@uncc.edu'}
-   return render_template('post.html', post=posts[int(post_id)], user=a_user)
+    a_user = db.session.query(User).filter_by(email='chua@uncc.edu').one()
+    my_post = db.session.query(Post).filter_by(id=post_id).one()
+    return render_template('post.html', post=my_post, user=a_user)
 
 
-@app.route('/notes/new', methods=['GET', 'POST'])
+@app.route('/posts/new', methods=['GET', 'POST'])
 def new_post():
-   a_user = {'name': 'Ana Pathak', 'email': 'apathak@uncc.edu'}
+    if request.method == 'POST':
+        title = request.form['title']
+        text = request.form['noteText']
+        from datetime import date
+        today = date.today()
+        today = today.strftime("%m-%d-%Y")
+        new_record = Post(title, text, today)
+        db.session.add(new_record)
+        db.session.commit()
 
-   if request.method == 'POST':
-       title = request.form['title']
-       text = request.form['noteText']
-       from datetime import date
-       today = date.today()
-       today = today.strftime("%m-%d-%Y")
-       id = len(posts) + 1
-       posts[id] = {'title': title, 'text': text, 'date': today}
-
-       return redirect(url_for('get_posts', name=a_user))
-   else:
-       return render_template('new.html', user=a_user)
+        return redirect(url_for('get_posts'))
+    else:
+        a_user = db.session.query(User).filter_by(email='chua@uncc.edu').one()
+        return render_template('new.html', user=a_user)
 
 
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)),debug=True)
