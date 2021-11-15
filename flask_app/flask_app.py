819ab49b4978c54883a780dae4332af2a88d48f7
chua <chua@uncc.edu>
2021-11-15 16:28:50 -0500

    added registration functionality

--- flask_app/flask_app.py
+++ flask_app/flask_app.py
@@ -5,14 +5,17 @@ import os                 # os is used to get environment variables IP & PORT
 from flask import Flask   # Flask is the web app that we will customize
 from flask import render_template
 from flask import request
-from flask import redirect, url_for
+from flask import redirect, url_for, session
 from database import db
 from models import Post as Post
 from models import User as User
+from forms import RegisterForm
+import bcrypt
 
 app = Flask(__name__)     # create an app
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flask_qna_app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
+app.config['SECRET_KEY'] = 'SE3155'
 #  Bind SQLAlchemy db object to this Flask app
 db.init_app(app)
 # Setup models
@@ -33,9 +36,11 @@ def index():
 
 @app.route('/posts')
 def get_posts():
-    a_user = db.session.query(User).filter_by(email='chua@uncc.edu').one()
-    posts = db.session.query(Post).all()
-    return render_template('posts.html', posts=posts, user=a_user)
+    if session.get('user'):
+        my_posts = db.session.query(Post).filter_by(user_id=session['user_id']).all()
+        return render_template('posts.html', posts=my_posts, user=session['user'])
+    else:
+        return redirect(url_for('login'))
 
 
 @app.route('/posts/<post_id>')
@@ -88,6 +93,31 @@ def delete_post(post_id):
     db.session.commit()
 
     return redirect(url_for('get_posts'))
+
+@app.route('/register', methods=['POST', 'GET'])
+def register():
+    form = RegisterForm()
+
+    if request.method == 'POST' and form.validate_on_submit():
+        # salt and hash password
+        h_password = bcrypt.hashpw(
+            request.form['password'].encode('utf-8'), bcrypt.gensalt())
+        # get entered user data
+        first_name = request.form['firstname']
+        last_name = request.form['lastname']
+        # create user model
+        new_user = User(first_name, last_name, request.form['email'], h_password)
+        # add user to database and commit
+        db.session.add(new_user)
+        db.session.commit()
+        # save the user's name to the session
+        session['user'] = first_name
+        session['user_id'] = new_user.id  # access id value from user model of this newly added user
+        # show user dashboard view
+        return redirect(url_for('get_posts'))
+
+    # something went wrong - display register view
+    return render_template('register.html', form=form)
 app.run(host=os.getenv('IP', '127.0.0.1'), port=int(os.getenv('PORT', 5000)),debug=True)
 
 # To see the web page in your web browser, go to the url,
